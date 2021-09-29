400c69011b5973f36b315624c5d2417140d118e4
joshuaaird <60545486+joshuaaird@users.noreply.github.com>
2021-09-29 15:44:51 -0400

    Update examplecall.py
    working test

--- examplecall.py
+++ examplecall.py
@@ -1,7 +1,10 @@
 from MakeLib import MakeLib
 from NFD_Lib import NFD_Lib
 
-#xout, yout = MakeLib('test/calibration.txt','test/ASCII_KR_left_8_tib.stl')
-print("Hiiii")
+xout, yout = MakeLib('','3D Models/tib/ASCII_KR_left_8_tib.stl')
+
+dc, mag, lib_angle, surface = NFD_Lib(xout,yout,1024)
+
+
 
 #hi
