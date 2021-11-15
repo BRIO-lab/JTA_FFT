7bd8fc5e02ae677f9aa26e3203f77fd3a8d2b9aa
ajensen1234 <ajensen123@ufl.edu>
2021-11-15 18:00:53 -0500

    Update call-actiyas.py

--- call-actiyas.py
+++ call-actiyas.py
@@ -3,21 +3,21 @@ import os
 import numpy as np
 
 def main():
-    ACTIYAS_DIR = "C:/Datasets_FemCleaned/Actiyas/Actiyas_Organized/T01R/Session_1/Cross_1"
-    FEM_STL = ACTIYAS_DIR + "/FemurR3_fem.stl"
+    GMK_DIR = "C:/Datasets_FemCleaned/GMK/GMK_Organized/Patient_2/Session_1/PivotExt_1"
+    FEM_STL = GMK_DIR + "/GMK_Sphere_right_5_fem.stl"
     FEM_NAME = os.path.splitext(FEM_STL)[0]
-    TIB_STL = ACTIYAS_DIR + "/TibiaR2_tib.stl"
+    TIB_STL = GMK_DIR + "/GMK_Sphere_right_5_tib.stl"
     TIB_NAME = os.path.splitext(TIB_STL)[0]
-    calibration = ACTIYAS_DIR + "/1024/cal1024.txt"
+    calibration = GMK_DIR + "/calibration.txt"
     
     NFD = JTA_FFT(calibration)
-    NFD.Make_Contour_Lib(calibration,FEM_STL,ACTIYAS_DIR)
-    NFD.Create_NFD_Library(ACTIYAS_DIR,"fem")
+    NFD.Make_Contour_Lib(FEM_STL)
+    NFD.Create_NFD_Library()
     NFD.save(FEM_NAME)
 
     NFD2 = JTA_FFT(calibration)
-    NFD2.Make_Contour_Lib(calibration, TIB_STL, ACTIYAS_DIR)
-    NFD2.Create_NFD_Library(ACTIYAS_DIR, "tib")
+    NFD2.Make_Contour_Lib(TIB_STL)
+    NFD2.Create_NFD_Library()
     NFD2.save(TIB_NAME)
     
 
