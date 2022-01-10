f2aaff4857537b4576ce9d10ea5fce5a3c3e9c9d
ajensen1234 <ajensen123@ufl.edu>
2022-01-10 11:32:01 -0500

    commit

--- call-actiyas.py
+++ call-actiyas.py
@@ -1,25 +1,32 @@
 from JTA_FFT import *
 import os
+import glob
 import numpy as np
 
 def main():
-    GMK_DIR = "C:\Datasets_FemCleaned/Lima/Lima Testing/Patient 77-13-PF/Session_1/Kneel_1"
-    FEM_STL = GMK_DIR + "/KR_right_6_fem.stl"
-    FEM_NAME = os.path.splitext(FEM_STL)[0]
-    TIB_STL = GMK_DIR + "/KR_right_6_tib.stl"
-    TIB_NAME = os.path.splitext(TIB_STL)[0]
-    calibration = GMK_DIR + "/calibration.txt"
-    
-    NFD = JTA_FFT(calibration)
-    NFD.Make_Contour_Lib(FEM_STL)
-    NFD.Create_NFD_Library()
-    NFD.save(FEM_NAME)
+    DATA_DIR = "C:/Datasets_FemCleaned/Toshi/Toshi_Organized/"
+    NFD_DIR = "C:/Datasets_FemCleaned/Toshi/NFD Library/lib/"
 
-    NFD2 = JTA_FFT(calibration)
-    NFD2.Make_Contour_Lib(TIB_STL)
-    NFD2.Create_NFD_Library()
-    NFD2.save(TIB_NAME)
-    
+    for pat_id in os.listdir(DATA_DIR):
+        
+        PAT_DIR = DATA_DIR + "/" + pat_id
+        for sess_id in os.listdir(PAT_DIR):
+            SESS_DIR = PAT_DIR + "/" + sess_id
+
+            for mvt_id in os.listdir(SESS_DIR):
+                MVT_DIR = SESS_DIR + "/" + mvt_id
+
+                cal = MVT_DIR + "/cal1024.txt"
+
+                STL_LIST = glob.glob1(MVT_DIR, "*.stl")
+                
+
+                for STL in STL_LIST:
+                    NFD = JTA_FFT(cal)
+                    NFD.Make_Contour_Lib(MVT_DIR +"/" +  STL)
+                    NFD.Create_NFD_Library()
+                    stl_name = os.path.splitext(STL)[0]
+                    NFD.save(NFD_DIR + stl_name)
 
 if __name__ == '__main__':
     main()
