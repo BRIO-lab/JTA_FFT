65bbb9ce133578396e2cb40d78d7b720ffe8f7b1
ajensen1234 <ajensen123@ufl.edu>
2022-01-11 10:18:17 -0500

    Update call-actiyas.py

--- call-actiyas.py
+++ call-actiyas.py
@@ -4,8 +4,8 @@ import glob
 import numpy as np
 
 def main():
-    DATA_DIR = "C:/Datasets_FemCleaned/Toshi/Toshi_Organized/"
-    NFD_DIR = "C:/Datasets_FemCleaned/Toshi/NFD Library/lib/"
+    DATA_DIR = "C:/Datasets_FemCleaned/Lima/Lima_Organized_Updated/"
+    NFD_DIR = "C:/Datasets_FemCleaned/Lima/NFD Library/lib/"
 
     for pat_id in os.listdir(DATA_DIR):
         
@@ -16,17 +16,18 @@ def main():
             for mvt_id in os.listdir(SESS_DIR):
                 MVT_DIR = SESS_DIR + "/" + mvt_id
 
-                cal = MVT_DIR + "/cal1024.txt"
+                cal = MVT_DIR + "/calibration.txt"
 
                 STL_LIST = glob.glob1(MVT_DIR, "*.stl")
                 
 
-                for STL in STL_LIST:
-                    NFD = JTA_FFT(cal)
-                    NFD.Make_Contour_Lib(MVT_DIR +"/" +  STL)
-                    NFD.Create_NFD_Library()
-                    stl_name = os.path.splitext(STL)[0]
-                    NFD.save(NFD_DIR + stl_name)
+
+        for STL in STL_LIST:
+            NFD = JTA_FFT(cal)
+            NFD.Make_Contour_Lib(MVT_DIR +"/" +  STL)
+            NFD.Create_NFD_Library()
+            stl_name = os.path.splitext(STL)[0]
+            NFD.save(NFD_DIR + stl_name)
 
 if __name__ == '__main__':
     main()
