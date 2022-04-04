96feb3e1b99bb602829e980bd1d268922cedf238
danieltorrejon <90070693+danieltorrejon@users.noreply.github.com>
2022-04-04 13:44:09 -0400

    Trying to fix an error

--- testing2.py
+++ testing2.py
@@ -1,23 +1,17 @@
-import os
-print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
-print("PATH:", os.environ.get('PATH'))
+import multiprocessing
+import numpy as np
+import time
 
+def sumall(value):
+    return sum(range(1, value + 1))
 
-# import multiprocessing
-# import numpy as np
-# import time
-
-# def sumall(value):
-#     return sum(range(1, value + 1))
-
-
-# if __name__ == '__main__':
+if __name__ == '__main__':
     
-#     start_time = time.time()
+    start_time = time.time()
 
-#     my_array = np.full((1, 10), 100)
+    my_array = np.full((1, 10), 100)
     
-#     pool_obj = multiprocessing.Pool()
-#     answer = pool_obj.map(sumall,range(0,100))
-#     print(my_array)
-#     print(time.time() - start_time)
+    pool_obj = multiprocessing.Pool()
+    answer = pool_obj.map(sumall,range(0,100))
+    print(my_array)
+    print(time.time() - start_time)
