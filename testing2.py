af0cb4dc35c24578d248a6b8a712afbc8c7cbd83
danieltorrejon <90070693+danieltorrejon@users.noreply.github.com>
2022-04-04 11:52:54 -0400

    Update testing2.py

--- testing2.py
+++ testing2.py
@@ -1,9 +1,23 @@
-import multiprocessing
+import os
+print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
+print("PATH:", os.environ.get('PATH'))
 
-def sumall(value):
-    return sum(range(1, value + 1))
 
-pool_obj = multiprocessing.Pool()
+# import multiprocessing
+# import numpy as np
+# import time
 
-answer = pool_obj.map(sumall,range(0,5))
-print(answer)
+# def sumall(value):
+#     return sum(range(1, value + 1))
+
+
+# if __name__ == '__main__':
+    
+#     start_time = time.time()
+
+#     my_array = np.full((1, 10), 100)
+    
+#     pool_obj = multiprocessing.Pool()
+#     answer = pool_obj.map(sumall,range(0,100))
+#     print(my_array)
+#     print(time.time() - start_time)
